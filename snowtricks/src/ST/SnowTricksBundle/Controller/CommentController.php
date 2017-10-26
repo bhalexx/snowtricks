006e48352680b43c4268a62829226ce53722c5bd
Bhalexx <minia-ture@hotmail.com>
2017-10-26 19:28:30 +0200

    Comments management

--- snowtricks/src/ST/SnowTricksBundle/Controller/CommentController.php
+++ snowtricks/src/ST/SnowTricksBundle/Controller/CommentController.php
@@ -20,8 +20,7 @@ class CommentController extends Controller
 	public function listAction(Request $request, Trick $trick, $page)
 	{
 		//This request is only callable by Ajax
-		if ($request->isXmlHttpRequest())
-        {
+		if ($request->isXmlHttpRequest()) {
 			$nbPerPage = 10;
 			$nbPages;
             
@@ -37,7 +36,7 @@ class CommentController extends Controller
             $nbPages = ceil(count($comments) / $nbPerPage);
 
             return new JsonResponse(array(
-            	'lastPage' => $nbPages - 1 <= (int)$page, 
+            	'lastPage' => $nbPages - 1 <= (int)$page,
             	'view' => $view
         	));
         }
@@ -69,11 +68,70 @@ class CommentController extends Controller
             $form = $this->get('form.factory')->create(CommentType::class, $comment);
 
             $request->getSession()->getFlashBag()->add('success', 'Merci pour votre commentaire !');
+
+            return $this->redirectToRoute('st_snowtricks.trick_view', array('id' => $trick->getId(), 'slug' => $trick->getSlug()));
         }
 
 		return $this->render('STSnowTricksBundle:Comment:add.html.twig', array(
-			'trick' => $trick,
-			'form' => $form->createView()
+			'form' => $form->createView(),
+            'comment' => $comment
 		));
 	}
+
+    /**
+     * @Security("has_role('ROLE_ADMIN') or user == comment.getAuthor()")
+     */
+    public function editAction(Request $request, Comment $comment)
+    {
+        //This request is only callable by Ajax
+        if ($request->isXmlHttpRequest()) {
+            $form = $this->get('form.factory')->create(CommentType::class, $comment);
+            
+            if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+
+                //Return JSON response
+                $view = $this->renderView('STSnowTricksBundle:Comment:comment.html.twig', array(
+                    'comment' => $comment
+                ));
+            } else {
+                //Return JSON response
+                $view = $this->renderView('STSnowTricksBundle:Comment:edit.html.twig', array(
+                    'form' => $form->createView(),
+                    'comment' => $comment
+                ));
+            }           
+
+            return new JsonResponse(array(
+                'view' => $view,
+                'status' => $request
+            ));
+        }
+
+        //If request not called by Ajax
+        throw new NotFoundHttpException();
+    }
+
+    /**
+     * @Security("has_role('ROLE_ADMIN') or user == comment.getAuthor()")
+     */
+    public function deleteAction(Request $request, Comment $comment)
+    {
+        //This request is only callable by Ajax
+        if ($request->isXmlHttpRequest()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($comment);
+            $em->flush();
+
+            //Return JSON response
+            return new JsonResponse(array(
+                'status' => 'OK',
+                'feedback' => 'Commentaire supprimé'
+            ));
+        }
+
+        //If request not called by Ajax
+        throw new NotFoundHttpException();
+    }
 }
