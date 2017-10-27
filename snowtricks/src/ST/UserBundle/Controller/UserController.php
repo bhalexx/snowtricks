e505513126c1b906f5c0a2265ce0afd194fb9ad5
Bhalexx <minia-ture@hotmail.com>
2017-10-27 14:20:13 +0200

    Fixtures: users' profile picture

--- snowtricks/src/ST/UserBundle/Controller/UserController.php
+++ snowtricks/src/ST/UserBundle/Controller/UserController.php
@@ -5,20 +5,31 @@ namespace ST\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use ST\UserBundle\User;
 
 class UserController extends Controller
 {
+	/**
+	 * Removes user's profile picture
+	 * @return JsonResponse
+	 */
     public function removeProfilePictureAction()
     {
-    	$em = $this->getDoctrine()->getManager();
+    	//This request is only callable by Ajax
+		if ($request->isXmlHttpRequest()) {
+	    	$em = $this->getDoctrine()->getManager();
 
-    	$user = $this->getUser();
-    	$user->removeProfilePicture();
+	    	$user = $this->getUser();
+	    	$user->removeProfilePicture();
 
-		$em->persist($user);
-		$em->flush();
+			$em->persist($user);
+			$em->flush();
 
-    	return new JsonResponse(array('response' => 'deleted'));
+	    	return new JsonResponse(array('response' => 'deleted'));
+	    }
+
+	    //If request not called by Ajax
+        throw new NotFoundHttpException();
     }
 }
